5cc3c561b9d4916aaad14de62c97ca161e0e3538
hir0o <mk.9.q.kyu@gmail.com>
2021-08-21 20:57:45 +0900

    tracerを作成

---
+++ trace/tracer_test.go
@@ -0,0 +1,20 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Errorf("New() returned nil")
+	} else {
+		tracer.Trace("今日わ")
+		if buf.String() != "今日わ\n" {
+			t.Errorf("'%s'という文字列が出力されました。", buf.String())
+		}
+	}
+
+}
